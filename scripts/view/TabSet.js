ce3e039c34e8ac608a4fb954dd9f9937dfaeab70
Neal Dangerfield <neal.dangerfield@caplin.com>
2016-01-11 16:20:41 +0000

    active tabset

--- scripts/view/TabSet.js
+++ scripts/view/TabSet.js
@@ -132,9 +132,10 @@ class TabSet extends React.Component
         var showHeader = node.getName() != null;
         var header = null;
         var tabStrip = null;
+        var selectedTabsetClass = (this.props.node.getModel().getActiveTabset() === this.props.node)?" flexlayout__tabset-selected":"";
         if (showHeader)
         {
-            var header = <div className="flexlayout__tabset_header"onMouseDown={this.onMouseDown.bind(this)}>{node.getName()}</div>
+            var header = <div className={"flexlayout__tabset_header" + selectedTabsetClass} onMouseDown={this.onMouseDown.bind(this)}>{node.getName()}</div>
             var tabStrip = <div className="flexlayout__tab_header_outer"  style={{top:"20px"}} >
                 <div ref="header" className="flexlayout__tab_header_inner">
                     {tabs}
@@ -143,7 +144,7 @@ class TabSet extends React.Component
         }
         else
         {
-            var tabStrip = <div className="flexlayout__tab_header_outer"  style={{top:"0px"}}
+            var tabStrip = <div className={"flexlayout__tab_header_outer" + selectedTabsetClass}  style={{top:"0px"}}
                                 onMouseDown={this.onMouseDown.bind(this)}>
                 <div ref="header" className="flexlayout__tab_header_inner">
                     {tabs}
@@ -182,6 +183,7 @@ class TabSet extends React.Component
         {
             name = ": " + name;
         }
+        this.props.node.getModel().doAction(Actions.setActiveTabset(this.props.node));
         this.props.layout.dragStart(event, "Move tabset" + name ,this.props.node, null, this.onDoubleClick.bind(this));
     }
 
