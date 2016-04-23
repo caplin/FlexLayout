573e125f5312f7259b93aec5e8ef965678c366fb
nmd <ndanger61@gmail.com>
2016-04-23 16:58:56 +0100

    export additional classes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import RowNode from './model/RowNode'
 import SplitterNode from './model/SplitterNode'
 import TabNode from './model/TabNode'
 import TabSetNode from './model/TabSetNode'
+import DockLocation from './DockLocation'
+import Orientation from './Orientation'
+import DragDrop from './DragDrop'
+import Rect from './Rect'
 
 export {  Layout,
 	Actions,
@@ -14,5 +18,9 @@ export {  Layout,
 	RowNode,
 	SplitterNode,
 	TabNode,
-	TabSetNode
+	TabSetNode,
+	DockLocation,
+	Orientation,
+	DragDrop,
+	Rect
 };
