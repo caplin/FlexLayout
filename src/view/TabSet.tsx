a5d16fd4c72d0b633d5520e414b8978591f9dd68
nmd <ndanger61@gmail.com>
2021-04-02 12:40:17 +0100

    fix for tabbar getting scrolled left due to renaming tab

--- src/view/TabSet.tsx
+++ src/view/TabSet.tsx
@@ -25,6 +25,7 @@ export const TabSet = (props: ITabSetProps) => {
 
     const toolbarRef = React.useRef<HTMLDivElement | null>(null);
     const overflowbuttonRef = React.useRef<HTMLButtonElement | null>(null);
+    const tabbarInnerRef = React.useRef<HTMLDivElement | null>(null);
 
     const { selfRef, position, userControlledLeft, hiddenTabs, onMouseWheel } = useTabOverflow(node, Orientation.HORZ, toolbarRef);
 
@@ -74,6 +75,11 @@ export const TabSet = (props: ITabSetProps) => {
 
     // Start Render
     const cm = layout.getClassName;
+
+    // tabbar inner can get shifted left via tab rename, this resets scrollleft to 0
+    if (tabbarInnerRef.current !== null && tabbarInnerRef.current!.scrollLeft !== 0) {
+        tabbarInnerRef.current.scrollLeft = 0;
+    }
 
     const selectedTabNode: TabNode = node.getSelectedNode() as TabNode;
     let style = node._styleWithPosition();
@@ -213,7 +219,7 @@ export const TabSet = (props: ITabSetProps) => {
 
         tabStrip = (
             <div className={tabStripClasses} style={tabStripStyle} onMouseDown={onMouseDown} onTouchStart={onMouseDown}>
-                <div className={cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER) + " " + cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER_ + node.getTabLocation())}>
+                <div ref={tabbarInnerRef} className={cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER) + " " + cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER_ + node.getTabLocation())}>
                     <div
                         style={{ left: position }}
                         className={cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER_TAB_CONTAINER) + " " + cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER_TAB_CONTAINER_ + node.getTabLocation())}
@@ -232,7 +238,7 @@ export const TabSet = (props: ITabSetProps) => {
         }
         tabStrip = (
             <div className={tabStripClasses} style={tabStripStyle} onMouseDown={onMouseDown} onTouchStart={onMouseDown}>
-                <div className={cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER) + " " + cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER_ + node.getTabLocation())}>
+                <div ref={tabbarInnerRef} className={cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER) + " " + cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER_ + node.getTabLocation())}>
                     <div
                         style={{ left: position }}
                         className={cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER_TAB_CONTAINER) + " " + cm(CLASSES.FLEXLAYOUT__TABSET_TABBAR_INNER_TAB_CONTAINER_ + node.getTabLocation())}
