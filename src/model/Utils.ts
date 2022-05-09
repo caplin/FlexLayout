3475cb9d036192157c0492737180c61a0402c46e
nmd <ndanger61@gmail.com>
2022-05-09 16:15:04 +0100

    -

--- src/model/Utils.ts
+++ src/model/Utils.ts
@@ -48,25 +48,21 @@ export function adjustSelectedIndexAfterDock(node: TabNode) {
 export function adjustSelectedIndex(parent: TabSetNode | BorderNode | RowNode, removedIndex: number) {
     // for the tabset/border being removed from set the selected index
     if (parent !== undefined && (parent.getType() === TabSetNode.TYPE || parent.getType() === BorderNode.TYPE)) {
-        if (parent.getType() === BorderNode.TYPE) {
-            parent._setSelected(-1);
-        } else {
-            const selectedIndex = (parent as TabSetNode | BorderNode).getSelected();
-            if (selectedIndex !== -1) {
-                if (removedIndex === selectedIndex && parent.getChildren().length > 0) {
-                    if (removedIndex >= parent.getChildren().length) {
-                        // removed last tab; select new last tab
-                        parent._setSelected(parent.getChildren().length - 1);
-                    } else {
-                        // leave selected index as is, selecting next tab after this one
-                    }
-                } else if (removedIndex < selectedIndex) {
-                    parent._setSelected(selectedIndex - 1);
-                } else if (removedIndex > selectedIndex) {
-                    // leave selected index as is
+        const selectedIndex = (parent as TabSetNode | BorderNode).getSelected();
+        if (selectedIndex !== -1) {
+            if (removedIndex === selectedIndex && parent.getChildren().length > 0) {
+                if (removedIndex >= parent.getChildren().length) {
+                    // removed last tab; select new last tab
+                    parent._setSelected(parent.getChildren().length - 1);
                 } else {
-                    parent._setSelected(-1);
+                    // leave selected index as is, selecting next tab after this one
                 }
+            } else if (removedIndex < selectedIndex) {
+                parent._setSelected(selectedIndex - 1);
+            } else if (removedIndex > selectedIndex) {
+                // leave selected index as is
+            } else {
+                parent._setSelected(-1);
             }
         }
     }
