298bc247f49bb7022048a6a70cbf785dd4a7a707
nmd <ndanger61@gmail.com>
2022-05-09 15:24:01 +0100

    fix border enableAutoHide right position marker location

--- src/view/Layout.tsx
+++ src/view/Layout.tsx
@@ -517,7 +517,7 @@ export class Layout extends React.Component<ILayoutProps, ILayoutState> {
             edges.push(<div key="North" style={{ top: r.y, left: r.x + r.width / 2 - offset, width: length, height: width, borderBottomLeftRadius: radius, borderBottomRightRadius: radius }} className={className}></div>)
             edges.push(<div key="West" style={{ top: r.y + r.height / 2 - offset, left: r.x, width: width, height: length, borderTopRightRadius: radius, borderBottomRightRadius: radius }} className={className}></div>)
             edges.push(<div key="South" style={{ top: r.y + r.height - width, left: r.x + r.width / 2 - offset, width: length, height: width, borderTopLeftRadius: radius, borderTopRightRadius: radius }} className={className}></div>)
-            edges.push(<div key="East" style={{ top: r.y + r.height / 2 - offset, right: r.x, width: width, height: length, borderTopLeftRadius: radius, borderBottomLeftRadius: radius }} className={className}></div>)
+            edges.push(<div key="East" style={{ top: r.y + r.height / 2 - offset, left: r.x + r.width - width, width: width, height: length, borderTopLeftRadius: radius, borderBottomLeftRadius: radius }} className={className}></div>)
         }
 
         // this.layoutTime = (Date.now() - this.start);
